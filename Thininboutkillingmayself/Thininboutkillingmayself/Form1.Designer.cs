5cbde4f74a47716793b9ecf4361f495460de8dcc
Logan <lthun810@learner.dvusd.org>
2025-01-14 08:58:26 -0700

    thinkin about killin mayself

---
+++ Thininboutkillingmayself/Thininboutkillingmayself/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Thininboutkillingmayself
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxBit5 = new System.Windows.Forms.TextBox();
+            this.textBoxBit4 = new System.Windows.Forms.TextBox();
+            this.textBoxBit3 = new System.Windows.Forms.TextBox();
+            this.textBoxBit2 = new System.Windows.Forms.TextBox();
+            this.textBoxBit1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBit6 = new System.Windows.Forms.TextBox();
+            this.textBoxBit7 = new System.Windows.Forms.TextBox();
+            this.textBoxBit8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxBit5
+            // 
+            this.textBoxBit5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit5.Location = new System.Drawing.Point(361, 115);
+            this.textBoxBit5.MaxLength = 1;
+            this.textBoxBit5.Name = "textBoxBit5";
+            this.textBoxBit5.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit5.TabIndex = 0;
+            this.textBoxBit5.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit4
+            // 
+            this.textBoxBit4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit4.Location = new System.Drawing.Point(388, 115);
+            this.textBoxBit4.MaxLength = 1;
+            this.textBoxBit4.Name = "textBoxBit4";
+            this.textBoxBit4.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit4.TabIndex = 1;
+            this.textBoxBit4.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit3
+            // 
+            this.textBoxBit3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit3.Location = new System.Drawing.Point(415, 115);
+            this.textBoxBit3.MaxLength = 1;
+            this.textBoxBit3.Name = "textBoxBit3";
+            this.textBoxBit3.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit3.TabIndex = 2;
+            this.textBoxBit3.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit2
+            // 
+            this.textBoxBit2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit2.Location = new System.Drawing.Point(442, 115);
+            this.textBoxBit2.MaxLength = 1;
+            this.textBoxBit2.Name = "textBoxBit2";
+            this.textBoxBit2.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit2.TabIndex = 3;
+            this.textBoxBit2.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit1
+            // 
+            this.textBoxBit1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit1.Location = new System.Drawing.Point(469, 115);
+            this.textBoxBit1.MaxLength = 1;
+            this.textBoxBit1.Name = "textBoxBit1";
+            this.textBoxBit1.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit1.TabIndex = 5;
+            this.textBoxBit1.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // textBoxBit6
+            // 
+            this.textBoxBit6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit6.Location = new System.Drawing.Point(334, 115);
+            this.textBoxBit6.MaxLength = 1;
+            this.textBoxBit6.Name = "textBoxBit6";
+            this.textBoxBit6.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit6.TabIndex = 7;
+            this.textBoxBit6.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit7
+            // 
+            this.textBoxBit7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit7.Location = new System.Drawing.Point(307, 115);
+            this.textBoxBit7.MaxLength = 1;
+            this.textBoxBit7.Name = "textBoxBit7";
+            this.textBoxBit7.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit7.TabIndex = 8;
+            this.textBoxBit7.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // textBoxBit8
+            // 
+            this.textBoxBit8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBit8.Location = new System.Drawing.Point(280, 115);
+            this.textBoxBit8.MaxLength = 1;
+            this.textBoxBit8.Name = "textBoxBit8";
+            this.textBoxBit8.Size = new System.Drawing.Size(21, 22);
+            this.textBoxBit8.TabIndex = 9;
+            this.textBoxBit8.TextChanged += new System.EventHandler(this.textBoxBit5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxBit8);
+            this.Controls.Add(this.textBoxBit7);
+            this.Controls.Add(this.textBoxBit6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBit1);
+            this.Controls.Add(this.textBoxBit2);
+            this.Controls.Add(this.textBoxBit3);
+            this.Controls.Add(this.textBoxBit4);
+            this.Controls.Add(this.textBoxBit5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBit5;
+        private System.Windows.Forms.TextBox textBoxBit4;
+        private System.Windows.Forms.TextBox textBoxBit3;
+        private System.Windows.Forms.TextBox textBoxBit2;
+        private System.Windows.Forms.TextBox textBoxBit1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxBit6;
+        private System.Windows.Forms.TextBox textBoxBit7;
+        private System.Windows.Forms.TextBox textBoxBit8;
+    }
+}
+
---
+++ Thininboutkillingmayself/Thininboutkillingmayself/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thininboutkillingmayself
+{
+    public partial class Form1 : Form
+    {
+        int total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxBit5_TextChanged(object sender, EventArgs e)
+        {
+            total = 0; 
+            if (textBoxBit1.Text == "1")
+            {
+                total += 1;
+            }
+            if (textBoxBit2.Text == "1")
+            {
+                total += 2;
+            }
+            if (textBoxBit3.Text == "1")
+            {
+                total += 4;
+            }
+            if (textBoxBit4.Text == "1")
+            {
+                total += 8;
+            }
+            if (textBoxBit5.Text == "1")
+            {
+                total += 16;
+            }
+            if (textBoxBit6.Text == "1")
+            {
+                total += 32;
+            }
+            if (textBoxBit7.Text == "1")
+            {
+                total += 64;
+            }
+            if (textBoxBit8.Text == "1")
+            {
+                total += 128;
+            }
+            label1.Text = total.ToString();
+        }
+    }
+}
